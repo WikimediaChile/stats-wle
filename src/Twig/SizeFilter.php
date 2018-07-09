a3e534fe9402d22401ae7190d2b43aa88c1bcedc
dennistobar <dennis.tobar@gmail.com>
2018-07-09 12:47:50 -0400

    Añadiendo visión de los megapixeles

--- src/Twig/SizeFilter.php
+++ src/Twig/SizeFilter.php
@@ -12,15 +12,23 @@ class SizeFilter extends AbstractExtension
     public function getFilters()
     {
         return [
-          new TwigFilter('bytes', [$this, 'convert'])
+          new TwigFilter('bytes', [$this, 'convertBytes']),
+          new TwigFilter('pixeles', [$this, 'convertPixeles'])
         ];
     }
 
-    public function convert(int $number) : string
+    public function convertBytes(int $number) : string
     {
         for ($i = 0; $number > (pow(1024, $i)); $i++);
         $numero = round($number/(pow(1024, $i-1)), 2);
         $dimension = self::DIM[$i-1];
         return sprintf('%s %sB', $numero, $dimension);
+    }
+
+    public function convertPixeles(string $dimension) : string
+    {
+        preg_match_all('/(\d{1,})×(\d{1,})/', $dimension, $matches, PREG_SET_ORDER, 0);
+        $numero = round(((int)$matches[0][0]*(int)$matches[0][1])/1e6, 2);
+        return sprintf('%s Mpx', $numero);
     }
 }
